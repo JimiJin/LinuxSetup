cb283d64d37acb69aa4da31054a296650982811f
Jimi Jin <rongxuaj@gmail.com>
2020-09-30 10:02:00 +0800

    Modify aira2c config

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ apt install vim
 apt install curl
 
 curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/.vimrc --output /root/.vimrc
+mkdir /root/.aria2
+touch /root/.aria2/aria2.session
+curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/aria2c.conf --output /root/.aria2/aria2.conf
