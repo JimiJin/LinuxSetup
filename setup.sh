62bfa717527c474fcd112256af07da0ea8518b58
Jimi Jin <rongxuaj@gmail.com>
2020-09-30 13:05:37 +0800

    Modify aria2 setting

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ apt install vim
 apt install curl
 
 curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/.vimrc --output /root/.vimrc
+
+
+###ARIA2###
+apt install aria2
 mkdir /root/.aria2
 touch /root/.aria2/aria2.session
 curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/aria2c.conf --output /root/.aria2/aria2.conf
+aria2c --conf-path=/root/.aria2/aria2.conf
