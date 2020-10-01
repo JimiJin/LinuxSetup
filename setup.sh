6b09042ea988205f45091ec5b14b5746277ecb1a
Jimi Jin <rongxuaj@gmail.com>
2020-10-02 00:07:33 +0800

    Modify setup

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@ apt install curl
 
 curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/.vimrc --output /root/.vimrc
 
-###nginx###
-apt install nginx
-
 
 ###ARIA2###
 apt install aria2
@@ -16,14 +13,12 @@ touch /root/.aria2/aria2.session
 curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/aria2c.conf --output /root/.aria2/aria2.conf
 aria2c --conf-path=/root/.aria2/aria2.conf
 
-mkdir webui-aria2
-mkdir webui-aria2/docs
-#curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/index.html --output /root/webui-aria2/docs/index.html
-
-#curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/nginx.conf --output /etc/nginx/nginx.conf
-
-curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/index.html --output /usr/share/nginx/html/index.html
+###ARIA2 UI###
+mkdir -p /www/aria2-webui
+curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/index.html --output /www/aria2-webui/index.html
 
 
-systemctl stop nginx
-systemctl start nginx
+###nginx###
+apt install nginx
+curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/nginx.conf --output /etc/nginx/nginx.conf
+nginx -s reload
