3012bd2ba3d0f9ae0ebb30b7f6fa2f4297750669
Jimi Jin <rongxuaj@gmail.com>
2020-10-01 13:27:48 +0800

    Modify nginx config

--- setup.sh
+++ setup.sh
@@ -16,5 +16,14 @@ touch /root/.aria2/aria2.session
 curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/aria2c.conf --output /root/.aria2/aria2.conf
 aria2c --conf-path=/root/.aria2/aria2.conf
 
+mkdir webui-aria2
 mkdir webui-aria2/docs
-curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/index.html --output /root/webui-aria2/docs/index.html
+#curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/index.html --output /root/webui-aria2/docs/index.html
+
+#curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/nginx.conf --output /etc/nginx/nginx.conf
+
+curl https://raw.githubusercontent.com/JimiJin/LinuxSetup/master/index.html --output /usr/share/nginx/html/index.html
+
+
+systemctl stop nginx
+systemctl start nginx
